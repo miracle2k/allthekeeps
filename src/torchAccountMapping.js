bca217880c35541c06663b1b9395e1bc5def3e3c
Antonio Salazar Cardozo <savedfastcool@gmail.com>
2020-10-01 16:52:58 -0400

    Added tweets for tBTC zksync torch carriers to date

--- src/torchAccountMapping.js
+++ src/torchAccountMapping.js
@@ -1,3 +1,15 @@
 export Mapping = {
-    '0xc2c00cfaf708b44932a3172b10b2f22b891b95c9': 'ercwl'
+    '0xb6f512124d8ddb8d7ef8559318de92531af32f6f': 'https://twitter.com/ercwl/status/1311452208127447044',
+    '0xc2c00cfaf708b44932a3172b10b2f22b891b95c9': 'https://twitter.com/bcmakes/status/1311482851469094914',
+    '0x6d64c48259056248e7629f2f8b306a8dec6ebc84': 'https://twitter.com/calebtebbe/status/1311486631610707968',
+    '0x425249cf0f2f91f488e24cf7b1aa3186748f7516': 'https://twitter.com/_prestwich/status/1311506768988532737',
+    '0x0e98e6c1b32f61ed8396a11730ac2f7d46e9cb8b': 'https://twitter.com/eKRENZKE/status/1311515387159175168',
+    '0xb9ab101eb21561ee343f88271654986dbd397070': 'https://twitter.com/gakonst/status/1311652009582493697',
+    '0xf253266c062014482b7b00473ca3b63211ad913e': 'https://twitter.com/gtklocker/status/1311707875358711809',
+    '0xe1b06c08aa24d03bec4ac4799f7aadd183a22e5c': 'https://twitter.com/ViktorBunin/status/1311728473074003969',
+    '0xc8e0345596d7196941e61d3ab607e57fe61f85e7': 'https://twitter.com/OLeveraging/status/1311731142429679621',
+    '0x0536e7dd2f63f5bbe2b581b704f98f8716e40b58': 'https://twitter.com/DZack23/status/1311732655491354624',
+    '0x7889296d06cd861fe6e2cd9cc671e488259ba3cf': 'https://twitter.com/pedrouid/status/1311742902998097924',
+    '0xd0769849c3af28a9df5a89e602c013853eb674ed': 'https://twitter.com/lightcoin/status/1311752269818458114',
+    '0x5e349eca2dc61abcd9dd99ce94d04136151a09ee': 'https://twitter.com/lightcoin/status/1311762132862668800'
 }
