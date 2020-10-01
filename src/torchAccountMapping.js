ba3202e03c0bddf915657e0d1c40595c76d873bc
Antonio Salazar Cardozo <savedfastcool@gmail.com>
2020-10-01 18:35:43 -0400

    Added more torchbearers 🔥

--- src/torchAccountMapping.js
+++ src/torchAccountMapping.js
@@ -11,5 +11,10 @@ export Mapping = {
     '0x0536e7dd2f63f5bbe2b581b704f98f8716e40b58': 'https://twitter.com/DZack23/status/1311732655491354624',
     '0x7889296d06cd861fe6e2cd9cc671e488259ba3cf': 'https://twitter.com/pedrouid/status/1311742902998097924',
     '0xd0769849c3af28a9df5a89e602c013853eb674ed': 'https://twitter.com/lightcoin/status/1311752269818458114',
-    '0x5e349eca2dc61abcd9dd99ce94d04136151a09ee': 'https://twitter.com/lightcoin/status/1311762132862668800'
+    '0x5e349eca2dc61abcd9dd99ce94d04136151a09ee': 'https://twitter.com/ljxie/status/1311765940674142211',
+    '0x794aa628bd7b968051d4a1c6273f96f350a4149b': 'https://twitter.com/Melt_Dem/status/1311762743750467591',
+    '0x725e86fe265e61b6ac4f87e36d7f353282800721': 'https://twitter.com/Obstropolos/status/1311770680707473409',
+    '0x07C6f80d694Db95E697Aea80708D6FE7DCF1e8e8': 'https://twitter.com/wycdd/status/1311773359802286092',
+    '0x13C0DabEb938c21524C59CDc40BCB6fDB3618754': 'https://twitter.com/ti_bi_ke/status/1311784527648391169',
+    '0x72a0d01ab425b9337f1c156cca9cdc42ea66d03c': 'https://twitter.com/ti_bi_ke/status/1311787176502398977'
 }
